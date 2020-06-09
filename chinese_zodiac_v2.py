c89f88269a2d619eac4b33586cb728d74b22e5c2
acswift <berthua@foxmail.com>
2020-06-09 17:23:50 +0800

    for 循环

--- chinese_zodiac_v2.py
+++ chinese_zodiac_v2.py
@@ -14,3 +14,12 @@ print(chinese_zodiac[year % 12])
 
 if chinese_zodiac[year % 12] == '狗':
     print('狗年运势大吉大利')
+
+for cz in chinese_zodiac:
+    print(cz)
+
+for i in range(1, 13):
+    print(i)
+
+for year in range(2000, 2019):
+    print('%s 年的生肖是 %s' % (year, chinese_zodiac[year % 12]))
